e2d1d8081e509a179b32498d636f3eb1f43e10d4
Katie Wilson <kmcwilson4@gmail.com>
2023-01-24 11:11:23 -0500

    styling home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,9 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="container" style={{ minHeight: '60vh' }}>
-         <div>
-                <img className="flex-item about-image"src="whistler.jpg" alt="Katie smiling" height="400px" width="400px"></img>
-            </div>   
             <div className="home-info flex-item">
                 <h2>Hey! I'm Katie</h2>
+                <img className="flex-item home-image"src="whistler.jpg" alt="Katie smiling" height="250px" width="200px" style={{float:'left'}}></img>
                 <p className="home-description"> I am a full stack web developer and former elementary school teacher with a passion for building creative, attractive and responsive web applications.</p>
                 <a href='Kathleen-Wilson-Resume_2022(10).pdf' download='Kathleen-Wilson-Resume_2022(10).pdf'><button className="card-button">Download Resume</button></a>
                 <a href='/#about'><button className="card-button" type='submit'>More About Me</button></a>
