5c46c8c7fadf89a67dac39e581d997f352a058ee
Katie Wilson <kmcwilson4@gmail.com>
2023-01-24 16:27:25 -0500

    added email icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavBar from './Navigation';
-import {FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import {BsFillEnvelopeFill} from 'react-icons/bs';
 
 const Header = ({ currentPage, setCurrentPage }) => {
     return (
         <header>
-            <div className='container' style={{marginBottom: '30px'}}>
-                <h1 className= 'portfolio-title'style= {{paddingTop: '20px'}}>Katie Wilson <a href="https://github.com/kmcwilson" style={{color:'#935c68ff'}}><FaGithub/></a> <a href=" https://www.linkedin.com/in/kathleen-wilson-2b2a8b54" style={{color:'#935c68ff'}}><FaLinkedin/></a> </h1>
+            <div className='container' style={{ marginBottom: '30px' }}>
+                <h1 className='portfolio-title' style={{ paddingTop: '20px' }}>Katie Wilson <a href="https://github.com/kmcwilson" style={{ color: '#935c68ff' }}><FaGithub /></a> <a href=" https://www.linkedin.com/in/kathleen-wilson-2b2a8b54" style={{ color: '#935c68ff' }}><FaLinkedin /></a>  <a style={{ color: '#935c68ff' }} class="email" href="mailto:kmcwilson4@gmail.com"><BsFillEnvelopeFill/></a></h1>
                 <NavBar setCurrentPage={setCurrentPage} currentPage={currentPage} id="navigation" className="flex-item nav-links-container" />
             </div>
         </header>
