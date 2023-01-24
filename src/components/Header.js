e37a6198bec93f6df8a02a99dbcf8065b146dcd6
Katie Wilson <kmcwilson4@gmail.com>
2023-01-24 16:16:40 -0500

    added to icons for page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from './Navigation';
+import {FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Header = ({ currentPage, setCurrentPage }) => {
     return (
         <header>
             <div className='container' style={{marginBottom: '30px'}}>
-                <h1 className= 'portfolio-title'style= {{paddingTop: '20px'}}>Katie Wilson Portfolio</h1>
+                <h1 className= 'portfolio-title'style= {{paddingTop: '20px'}}>Katie Wilson <a href="https://github.com/kmcwilson" style={{color:'#935c68ff'}}><FaGithub/></a> <a href=" https://www.linkedin.com/in/kathleen-wilson-2b2a8b54" style={{color:'#935c68ff'}}><FaLinkedin/></a> </h1>
                 <NavBar setCurrentPage={setCurrentPage} currentPage={currentPage} id="navigation" className="flex-item nav-links-container" />
             </div>
         </header>
