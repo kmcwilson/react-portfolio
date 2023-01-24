389847bcd2fb6548f7a10b9a87db77a843bce574
Katie Wilson <kmcwilson4@gmail.com>
2023-01-24 10:32:05 -0500

    added pictures

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -4,7 +4,7 @@ const AboutMe = () => {
     return (
         <div style={{ minHeight: '60vh', textAlign: 'center' }} className="container about-myself">
             <div>
-                <img className="flex-item about-image"src="joffree-lake.jpg" alt="Katie smiling" height="300px" width="300px"></img>
+                <img className="flex-item about-image"src="whistler.jpg" alt="Katie smiling" height="300px" width="300px"></img>
             </div>
             <div>
                 <p className="flex-item about-info">Future Full Stack Web Developer leveraging background in Education to provide insight into how users will interact with software programs and websites as well as how these programs will be perceived by the user. Extensive experience in learning systems within the School Board coupled with the knowledge of Full Stack Web Development in order to successfully work within the technical realm of an institution. Effective at combining creativity and problem solving to develop user-friendly applications. Known among peers and colleagues for being a dedicated worker, a quick-learner and for paying attention to detail no matter the complexity of the project. </p>
